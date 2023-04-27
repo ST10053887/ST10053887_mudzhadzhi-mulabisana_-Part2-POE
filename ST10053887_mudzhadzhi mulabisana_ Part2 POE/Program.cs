20c002d9bc4784bf32c9bccfdb3082d41e921d7f
st10053887 <st10053887@rcconnect.edu.za>
2023-04-27 22:45:09 +0000

    added a while loop with a if statement

--- ST10053887_mudzhadzhi mulabisana_ Part2 POE/Program.cs
+++ ST10053887_mudzhadzhi mulabisana_ Part2 POE/Program.cs
@@ -15,6 +15,24 @@ namespace ST10053887_mudzhadzhi_mulabisana__Part2_POE
             string name = Console.ReadLine();
             Recipe recipe = new Recipe(name);
 
+            while (true)
+            {
+                Console.WriteLine("Enter an ingredient name (or 'done' to finish):");
+                string ingredientName = Console.ReadLine();
+                if (ingredientName.ToLower() == "done")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Enter the number of calories:");
+                int calories = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the food group:");
+                string foodGroup = Console.ReadLine();
+
+                recipe.AddIngredient(new Ingredient(ingredientName, calories, foodGroup));
+            }
+
         }
     }
 }
