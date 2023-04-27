4b2f6fee9a7113711bd6ef8a1017f5cfe143d9b0
st10053887 <st10053887@rcconnect.edu.za>
2023-04-27 22:42:46 +0000

    added a method

--- ST10053887_mudzhadzhi mulabisana_ Part2 POE/Program.cs
+++ ST10053887_mudzhadzhi mulabisana_ Part2 POE/Program.cs
@@ -8,8 +8,12 @@ namespace ST10053887_mudzhadzhi_mulabisana__Part2_POE
 {
     internal class Program
     {
+        static List<Recipe> recipes = new List<Recipe>();
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a recipe name:");
+            string name = Console.ReadLine();
+            Recipe recipe = new Recipe(name);
 
         }
     }
